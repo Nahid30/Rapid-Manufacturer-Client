6f16225aedbcaabbad21262b09b8bd2ef9adda73
Nahid30 <nahidshuvo30@gmail.com>
2022-05-26 07:27:36 +0600

    secure by admin route

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, Outlet } from 'react-router-dom';
 import auth from '../firebase.init';
+import useAdmin from '../hooks/useAdmin';
 
 const Dashboard = () => {
     const [user] = useAuthState(auth);
-    
+    const [admin] = useAdmin(user);
+
 
     return (
         <div>
@@ -20,17 +22,25 @@ const Dashboard = () => {
                     <label for="dashboard-sidebar" class="drawer-overlay"></label>
                     <ul class="menu p-4 overflow-y-auto w-60 bg-base-100 text-base-content">
                         <li><Link to="/dashboard/myProfile">My Profile</Link></li>
-                        <>
-                            <li><Link to="/dashboard">My Order</Link></li>
-                            <li><Link to="/dashboard/addReview">Add A Review</Link></li>
-                        </>
-                        
-                        <>
-                            <li><Link to="/dashboard/manageOrders">Manage All Orders</Link></li>
-                            <li><Link to="/dashboard/addProduct">Add A Product</Link></li>
-                            <li><Link to="/dashboard/makeAdmin">Make Admin</Link></li>
-                            <li><Link to="/dashboard/manageProducts">Manage Products</Link></li>
-                        </>
+
+                        {
+                            admin ?
+                                <>
+                                    <li><Link to="/dashboard/manageOrders">Manage All Orders</Link></li>
+                                    <li><Link to="/dashboard/addProduct">Add A Product</Link></li>
+                                    <li><Link to="/dashboard/makeAdmin">Make Admin</Link></li>
+                                    <li><Link to="/dashboard/manageProducts">Manage Products</Link></li>
+                                </>
+                                :
+                                <>
+                                    <li><Link to="/dashboard">My Order</Link></li>
+                                    <li><Link to="/dashboard/addReview">Add A Review</Link></li>
+                                </>
+                        }
+
+
+
+
 
 
 
